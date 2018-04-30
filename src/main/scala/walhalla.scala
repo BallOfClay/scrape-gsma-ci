b3532ada4372c87d22dde150c63c35e8ca8849b3
moocow <ghost@cigarplug.ml>
2018-04-30 21:58:26 +0530

    ability to return data from sql. w00t!

--- src/main/scala/walhalla.scala
+++ src/main/scala/walhalla.scala
@@ -1,6 +1,3 @@
-import java.io.File
-import java.util
-
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
 
@@ -9,13 +6,14 @@ import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
 import doobie._
 import doobie.implicits._
-// import cats._
+import cats._
 import cats.effect._
-// import cats.implicits._
+import cats.implicits._
 
-import com.typesafe.config.ConfigFactory
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
+
+import shapeless._
 
 
 
@@ -32,14 +30,11 @@ trait sqlCon {
 
 class oemTable extends sqlCon {
 
-  case class oemResource(name: String, count: String, location: String)
+  case class oemResource(name: String, count: Int, location: String)
 
   private val source : Elements  = {
     val sesh = Jsoup.connect("https://www.gsmarena.com/makers.php3").get()
-
-    val maker_nodes = sesh.select(".st-text a")
-
-    maker_nodes
+    sesh.select(".st-text a")
   }
 
 
@@ -49,25 +44,22 @@ class oemTable extends sqlCon {
 
   def getCounts: Array[String] = {
     val rex : Regex = """(?i)((?<=\s)\d+(?= device(s)))""".r
-
     source.eachText() flatMap (x => rex findFirstIn(x)) toArray
   }
 
   def getOEMs: Array[String] = {
     val rex : Regex = """(?i)(.*(?= \d+ devices))""".r
-
     source.eachText flatMap (x => rex findFirstIn(x)) toArray
   }
 
 
 
   val tmp  = {
-
-    val a = sql"select device_count from gsm_resources".query[String].to[List].transact(xa).unsafeRunSync()
-    a
+    val a = sql"select * from gsm_resources".query[oemResource]
+    a.to[List].transact(xa).unsafeRunSync()
   }
 
-  //val username: String = s"${conf.getString("username")}"
+
 
 }
 
@@ -78,6 +70,7 @@ object walhalla {
     val oem_table = new oemTable
 
     val oem_names = oem_table.tmp
+
     oem_names foreach(println)
 
     //println(oem_names)
